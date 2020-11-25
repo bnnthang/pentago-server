51ea8a4a1e84eaf6d71ffc84d1986015bb0aa503
ksk642 <buinnthang@gmail.com>
2020-11-25 07:03:59 +0700

    upd

--- server.py
+++ server.py
@@ -1,93 +1,132 @@
+import colorama
 import eventlet
-import socketio
 import os
 import random
+import socketio
+
 
 sio = socketio.Server()
 app = socketio.WSGIApp(sio, static_files={
-    '/': {'content_type': 'text/html', 'filename': 'index.html'}
+    "/": {"content_type": "text/html", "filename": "index.html"}
 })
 
 waitingQueue = []
 currentRooms = {}
+assignedRoom = {}
+
+
+def specialPrint(*args):
+    """
+    Function for debugging
+    """
+    st = " ".join(str(i) for i in args)
+    print(colorama.Back.MAGENTA, st)
+    print(colorama.Style.RESET_ALL)
 
 
 @sio.event
 def connect(sid, environ):
-    print(sid, "connected")
+    """
+    Handle when a new socket connected
+    """
+
+    # print debug
+    specialPrint(sid, "connected")
 
     # find oppo
     if len(waitingQueue) > 0:
         # if found
-        assignedRoom = waitingQueue[0]
+        oppo_sid = waitingQueue[0]
+        assignedRoomId = oppo_sid + sid
         # remove from queue
         waitingQueue.pop()
         # enter room
-        sio.enter_room(sid, assignedRoom)
-        sio.leave_room(sid, sid)
+        sio.enter_room(oppo_sid, assignedRoomId)
+        sio.enter_room(sid, assignedRoomId)
         # cache data
-        currentRooms[assignedRoom] = (sid, assignedRoom)
+        currentRooms[assignedRoomId] = (sid, oppo_sid)
+        assignedRoom[oppo_sid] = assignedRoomId
+        assignedRoom[sid] = assignedRoomId
         # set roles
         blackIndex = random.randint(0, 1)
         whiteIndex = 1 - blackIndex
-        sio.emit("role", "black", currentRooms[assignedRoom][blackIndex])
-        sio.emit("role", "white", currentRooms[assignedRoom][whiteIndex])
+        specialPrint("black:", currentRooms[assignedRoomId][blackIndex], "white:", currentRooms[assignedRoomId][whiteIndex])
+        sio.emit("role", "black", currentRooms[assignedRoomId][blackIndex])
+        sio.emit("role", "white", currentRooms[assignedRoomId][whiteIndex])
 
-        print("in rooms", currentRooms)
+        specialPrint("in rooms", currentRooms)
     else:
         # add to queue
         waitingQueue.append(sid)
-        print("in wait", waitingQueue)
+        specialPrint("in wait", waitingQueue)
 
 
 def relayMessage(sid, _event, message):
-    sio.emit(room=sio.rooms(sid)[0], event=_event, data=message)
+    sio.emit(room=assignedRoom[sid], event=_event, data=message)
+    specialPrint("emitted", message, "to room", assignedRoom[sid])
 
 
 @sio.on("bmove")
 def blackMoveRelay(sid, state):
-    print(sid, "moves", state)
+    specialPrint(sid, "moves", state)
     if int(state) == 0:
         return
     relayMessage(sid, "bmove", state)
 
+
 @sio.on("wmove")
 def whiteMoveRelay(sid, state):
-    print(sid, "moves", state)
+    specialPrint(sid, "moves", state)
     if int(state) == 0:
         return
     relayMessage(sid, "wmove", state)
 
+
 @sio.on("ended")
 def gameEnded(sid):
+    """
+    When the game ended
+    """
     # disconnect players
     sio.disconnect(sid)
 
+
 @sio.on("chat")
 def chatRelay(sid, msg):
-    print(sid, "said", msg)
+    specialPrint(sid, "said", msg)
     relayMessage(sid, "chat", msg)
 
-@sio.event
-def message(sid, data):
-    print(f"receive {data} from {sid}")
-    sio.send("hi there", room=sid)
 
 @sio.event
 def disconnect(sid):
-    print(sid, "disconnected")
+    """
+    Handle when a socket disconnect
+    """
 
-    assignedRoom = sio.rooms(sid)[0]
-    removeQueue = currentRooms.get(assignedRoom, [])
+    # print debug
+    specialPrint(sid, "disconnected")
+
+    # if still in queue
+    if sid in waitingQueue:
+        waitingQueue.pop(waitingQueue.index(sid))
+        return
+
+    # get the room id
+    assignedRoomId = assignedRoom[sid]
+    specialPrint("deleting", assignedRoom)
+
+    # disconnect other socket in room
+    removeQueue = currentRooms.get(assignedRoomId, [])
     for uid in removeQueue:
         sio.disconnect(uid)
+        assignedRoom.pop(uid)
     
+    # remove cache data
+    assignedRoom.pop(sid)
     currentRooms.pop(assignedRoom)
-    print("in rooms", currentRooms)
+    specialPrint("in rooms", currentRooms)
 
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     PORT = int(os.getenv("PORT"))
-    # print(PORT)
-    # PORT = 4602
     eventlet.wsgi.server(eventlet.listen(('', PORT)), app)
