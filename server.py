07ed9ed8a13e83a2ee9af6a4f4d70a5bfa5a4e14
ksk642 <buinnthang@gmail.com>
2020-11-26 14:46:14 +0700

    upd

--- server.py
+++ server.py
@@ -4,13 +4,16 @@ import os
 import random
 import socketio
 
-
 sio = socketio.Server()
-app = socketio.WSGIApp(sio, static_files={
-    "/": {"content_type": "text/html", "filename": "index.html"}
-})
+app = socketio.WSGIApp(sio,
+                       static_files={
+                           "/": {
+                               "content_type": "text/html",
+                               "filename": "index.html"
+                           }
+                       })
 
-waitingQueue = []
+waitingId = None
 currentRooms = {}
 assignedRoom = {}
 
@@ -20,8 +23,7 @@ def specialPrint(*args):
     Function for debugging
     """
     st = " ".join(str(i) for i in args)
-    print(colorama.Back.MAGENTA, st)
-    print(colorama.Style.RESET_ALL)
+    print(colorama.Back.BLUE, st, colorama.Style.RESET_ALL)
 
 
 @sio.event
@@ -30,16 +32,22 @@ def connect(sid, environ):
     Handle when a new socket connected
     """
 
+    global waitingId
+
     # print debug
     specialPrint(sid, "connected")
 
     # find oppo
-    if len(waitingQueue) > 0:
+    if waitingId == None:
+        # add to queue
+        waitingId = sid
+        specialPrint("in wait", waitingId)
+    else:
         # if found
-        oppo_sid = waitingQueue[0]
+        oppo_sid = waitingId
         assignedRoomId = oppo_sid + sid
         # remove from queue
-        waitingQueue.pop()
+        waitingId = None
         # enter room
         sio.enter_room(oppo_sid, assignedRoomId)
         sio.enter_room(sid, assignedRoomId)
@@ -50,15 +58,12 @@ def connect(sid, environ):
         # set roles
         blackIndex = random.randint(0, 1)
         whiteIndex = 1 - blackIndex
-        specialPrint("black:", currentRooms[assignedRoomId][blackIndex], "white:", currentRooms[assignedRoomId][whiteIndex])
+        specialPrint("black:", currentRooms[assignedRoomId][blackIndex],
+                     "white:", currentRooms[assignedRoomId][whiteIndex])
         sio.emit("role", "black", currentRooms[assignedRoomId][blackIndex])
         sio.emit("role", "white", currentRooms[assignedRoomId][whiteIndex])
 
         specialPrint("in rooms", currentRooms)
-    else:
-        # add to queue
-        waitingQueue.append(sid)
-        specialPrint("in wait", waitingQueue)
 
 
 def relayMessage(sid, _event, message):
@@ -87,6 +92,7 @@ def gameEnded(sid):
     """
     When the game ended
     """
+    specialPrint("ended")
     # disconnect players
     sio.disconnect(sid)
 
@@ -106,24 +112,30 @@ def disconnect(sid):
     # print debug
     specialPrint(sid, "disconnected")
 
+    global waitingId
+
     # if still in queue
-    if sid in waitingQueue:
-        waitingQueue.pop(waitingQueue.index(sid))
+    if sid == waitingId:
+        waitingId = None
         return
 
     # get the room id
     assignedRoomId = assignedRoom[sid]
     specialPrint("deleting", assignedRoom)
 
-    # disconnect other socket in room
+    # remove cache data
     removeQueue = currentRooms.get(assignedRoomId, [])
+    if sid in assignedRoom:
+        assignedRoom.pop(sid)
+    if assignedRoomId in currentRooms:
+        currentRooms.pop(assignedRoomId)
+
+    # disconnect other socket in room
     for uid in removeQueue:
         sio.disconnect(uid)
-        assignedRoom.pop(uid)
-    
-    # remove cache data
-    assignedRoom.pop(sid)
-    currentRooms.pop(assignedRoom)
+        if uid in assignedRoom:
+            assignedRoom.pop(uid)
+
     specialPrint("in rooms", currentRooms)
 
 
